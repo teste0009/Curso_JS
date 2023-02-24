efba9015bf4322f5c78e20356c912bc404a0b0b6
Gomez, Enzo <enzo.gomez@accenture.com>
2023-02-24 13:54:36 -0300

    Tips

--- assets/js/1-tip.js
+++ assets/js/1-tip.js
@@ -1,4 +1,4 @@
-// Funcion clasica
+// TIP 1: Funcion clasica
 // function crearPersona(nombre, apellido) {
 //     return {
 //         nombre: nombre,
@@ -11,3 +11,34 @@ const crearPersona = (nombre, apellido) => ({ nombre, apellido });
 
 const persona = crearPersona('Peter', 'Parker');
 console.log(persona);
+
+
+// TIP 2: Destructurar argumentos
+const imprimeArgumentos = (nombre, ...argumentos) => {
+  const [apellido, color, anio, esReal, saludo] = argumentos;
+  console.log('argumentos: ', apellido, color, anio, esReal, saludo);
+}
+
+imprimeArgumentos('Gonzalez', 'Nicolas', 'Rojo', 2023, true, 'Hola');
+
+
+const personaje = {
+  nombre: 'Tony Stark',
+  codeName: 'Ironman',
+  edad: 40,
+  // vivo: false,
+};
+
+// Destructurar Objeto
+const imprimeObjeto = (objeto) => {
+  const {
+    nombre: miPrimerNombre,
+    codeName,
+    edad,
+    vivo: estaVivo = true,
+  } = objeto;
+
+  console.log('objeto:', miPrimerNombre, codeName, edad, estaVivo);
+}
+
+imprimeObjeto(personaje);
